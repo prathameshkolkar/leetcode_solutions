7e07c26b6abd0c65d88b9eae33364c8b890b2e2d
prathameshkolkar <ptk34923@gmail.com>
2026-02-20 11:06:20 +0530

    Time: 43 ms (70.13%), Space: 19.3 MB (15.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,11 +1,22 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0 or n==1:
-            return n
-        dp = [0] * (n + 1)
-        dp[0] = 0
-        dp[1] = 1
+        #Tabulation method
+        # if n == 0 or n==1:
+        #     return n
+        # dp = [0] * (n + 1)
+        # dp[0] = 0
+        # dp[1] = 1
        
-        for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
+        # for i in range(2, n + 1):
+        #     dp[i] = dp[i - 1] + dp[i - 2]
+        # return dp[n]
+        #memoization method
+        dp=[0]*(n+1)
+        return self.rec(n,dp)
+    def rec(self,n,dp):
+        if n==0 or n==1:
+            return n
+        if dp[n]!=0:
+            return dp[n]
+        dp[n]=self.rec(n-1,dp)+self.rec(n-2,dp)
         return dp[n]
